7619a099cb0dd5f8b0022d5564477419346ccbd5
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-10-24 23:05:28 +0100

    add GardePage in servicepage

--- app/(pages)/service/page.tsx
+++ app/(pages)/service/page.tsx
@@ -2,11 +2,12 @@
 import React from 'react'
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 import { Slider } from '@/components/slider/Slider';
+import GardePage from '@/components/pageGarde/GardePage';
 
 export default function CardHoverEffectDemo() {
   return (
     <>
-    <Slider/>
+    <GardePage/>
     <h1 className='text-white text-center mt-10 mb-10 text-6xl'>Nos Services</h1>
     <div className="max-w-5xl mx-auto px-8">
        
