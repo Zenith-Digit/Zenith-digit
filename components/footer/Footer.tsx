a639046f46d3c8dd3bf1f3ab92f8978bc5d67e3d
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-11-14 21:44:44 +0100

    update footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import lightLogo from "@/components/navbar/logo.png";
 import darkLogo from "@/components/navbar/logodark.png";
 import Image from "next/image";
-import { FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaLinkedin, FaInstagram, FaEnvelope, FaMobile, FaIdCard } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
 
@@ -12,77 +12,77 @@ interface FooterProps {
 
 const Footer = ({ isDarkMode }: FooterProps) => {
   return (
-    <footer className="border-t-4 border-[#00fefb] text-center bottom-0">
-      <div className="mx-auto w-full  p-4 ">
-        <div className="md:flex md:justify-between">
+    <footer className="border-t-4 border-[#00fefb] bottom-0">
+      <div className="mx-auto w-full py-4">
+        <div className="md:flex md:justify-between px-4">
           {/* Logo */}
           <div className="md:mb-0">
             <Image
               src={isDarkMode ? darkLogo : lightLogo}  // Changement dynamique du logo
               alt="logo"
-              className="w-24 md:mt-10 mx-auto"
+              className="w-24 md:w-40 md:mt-10 mx-auto"
             />
           </div>
 
           {/* Navigation Links */}
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 text-start md:justify-between md:w-2/3">
-            <div>
-              <h2 className="text-xl text-[#00fefb] mb-4 md:text-3xl">Resources</h2>
-              <ul className=" sm:text-xl">
-                <li className="mb-4">
-                  <Link href="https://flowbite.com/" className="hover:underline">Flowbite</Link>
-                </li>
-                <li>
-                  <Link href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="text-xl text-[#00fefb] mb-4 md:text-3xl">Follow us</h2>
-              <ul className=" sm:text-xl">
-                <li className="mb-4">
-                  <Link href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</Link>
-                </li>
-                <li>
-                  <Link href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="text-xl text-[#00fefb] mb-4 md:text-3xl">Pages</h2>
-              <ul className=" sm:text-xl">
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 md:grid-cols-3  md:justify-between w-2/3">
+            <div className=" flex flex-col justify-start items-start">
+              <h2 className="text-xl text-[#00fefb] md:text-center mb-4 md:text-3xl">Pages</h2>
+              <ul className=" sm:text-xl flex flex-col items-start">
                 <li className="mb-2"><Link href={'./'} className="hover:underline">Accueil</Link></li>
                 <li className="mb-2"><Link href={'./apropos'} className='hover: hover:underline'>À propos</Link></li>
                 <li className="mb-2"><Link href={'./service'} className=' hover:underline'>Nos services</Link></li>
                 <li className="mb-2"><Link href={'./contact'} className=' hover:underline'>Contact</Link></li>
               </ul>
             </div>
+            <div className="  flex flex-col justify-start items-start">
+              <h2 className="text-xl text-[#00fefb] mb-4 md:text-3xl">Adresse</h2>
+              <ul className="flex flex-col items-start sm:text-xl">
+                <li className="mb-2"><Link href={'./apropos'} className='hover:underline'>Email: info@zenithdigit.com</Link></li>
+                <li className="mb-2"><Link href={'./service'} className='hover:underline'>Whatsapp: +227 9700000</Link></li>
+              </ul>
+            </div>
+            <div className="  flex flex-col justify-start items-start">
+              <h2 className="text-xl text-[#00fefb] mb-4 md:text-3xl">Reseaux sociaux</h2>
+              <ul className="flex flex-col items-start sm:text-xl">
+                <li className="mb-2 flex hover:text-[#00fefb]">
+                  <Link href="https://www.facebook.com" className="sm:text-4xl me-3 sm:mt-1 md:mt-0" aria-label="Facebook">
+                    <FaFacebook />
+                  </Link>
+                  Facebook
+                </li>
+                <li className="mb-2 flex hover:text-[#00fefb]">
+                  <Link href="https://www.linkedin.com" className=" sm:text-4xl me-3 sm:mt-1 md:mt-0" aria-label="LinkedIn">
+                    <FaLinkedin />
+                  </Link>
+                  Linkedin
+                </li>
+                <li className="mb-2 flex hover:text-[#00fefb]">
+                  <Link href="https://www.instagram.com" className="sm:text-4xl me-3 sm:mt-1 md:mt-0" aria-label="Instagram">
+                    <FaInstagram />
+                  </Link>
+                  Instagram
+                </li>
+                <li className="mb-2 flex hover:text-[#00fefb]">
+                  <Link href="https://www.twitter.com" className="sm:text-4xl me-3 sm:mt-1 md:mt-0" aria-label="X Twitter">
+                    <FaXTwitter />
+                  </Link>
+                  Twitter
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
         {/* Separator Line */}
-        <div className="w-full my-5">
-          <hr className="border-t border-[#00fefb]" />
+        <div className="w-full my-5 p-O mx-0">
+          <hr className="border-t border-[#00fefb] m-0 p-0" />
         </div>
 
         {/* Footer Bottom Text */}
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm sm:text-lg sm:text-center">© 2024 <Link href="https://flowbite.com/" className="hover:underline">ZenithDigit™</Link>. All Rights Reserved.
+        <div className="text-center w-full">
+          <span className="text-sm sm:text-lg">© 2024 <Link href="/" className="hover:underline">ZenithDigit™</Link>. All Rights Reserved.
           </span>
-          <div className="flex mt-4 w-1/3 lg:w-1/5 mx-auto justify-between sm:mt-0 ">
-            <Link href="https://www.facebook.com" className="sm:text-4xl hover:text-[#00fefb]" aria-label="Facebook">
-              <FaFacebook />
-            </Link>
-            <Link href="https://www.linkedin.com" className=" sm:text-4xl hover:text-[#00fefb]" aria-label="LinkedIn">
-              <FaLinkedin />
-            </Link>
-            <Link href="https://www.instagram.com" className="sm:text-4xl hover:text-[#00fefb]" aria-label="Instagram">
-              <FaInstagram />
-            </Link>
-            <Link href="https://www.twitter.com" className="sm:text-4xl hover:text-[#00fefb]" aria-label="X Twitter">
-              <FaXTwitter />
-            </Link>
-          </div>
         </div>
       </div>
     </footer>
