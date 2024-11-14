6a4a894215d22f5bc536635344ca86aadca1fb6f
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-11-14 15:21:38 +0100

    adjust text

--- components/ourvision/VisionSection.tsx
+++ components/ourvision/VisionSection.tsx
@@ -8,12 +8,12 @@ export function  VisionSection() {
       <div className="lg:flex max-w-5xl mx-auto px-8 ">
         <div className="lg:w-1/2 lg:mt-36">
       <h2 className="text-[#00fefb] text-4xl font-bold p-8">Notre vision</h2>
-      <p className=" w-5/6 mx-auto py-4 text-white">Nous aspirons à devenir un partenaire de confiance pour les entreprises, en les dotant des outils et des stratégies qui leur permettent de se démarquer et de grandir durablement dans l’environnement digital.</p>
+      <p className=" w-5/6 mx-auto py-4 text-white text-xl">Nous aspirons à devenir un partenaire de confiance pour les entreprises, en les dotant des outils et des stratégies qui leur permettent de se démarquer et de grandir durablement dans l’environnement digital.</p>
       
       </div>
       <div className="lg:w-1/2 lg:mt-36">
       <h2 className="text-[#00fefb] text-4xl font-bold m-8">Notre Mission</h2>
-      <p className=" w-5/6 mx-auto py-4 text-white">Notre mission est de rendre la transformation digitale accessible à toutes les entreprises, peu importe leur taille, pour les aider à prospérer dans un monde numérique en constante évolution.</p>
+      <p className=" w-5/6 mx-auto py-4 text-white text-xl">Notre mission est de rendre la transformation digitale accessible à toutes les entreprises, peu importe leur taille, pour les aider à prospérer dans un monde numérique en constante évolution.</p>
       </div>
       </div>
       </div>
